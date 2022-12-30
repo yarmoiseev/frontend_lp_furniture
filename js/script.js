3d33646a3e13b8e5525f97d2ed7c1fe910c04d4c
Iaroslav Moiseev <yarmoiseev01@gmail.com>
2022-12-30 16:56:50 +0400

    ADD ANCOR LINKS

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 /* Hamburger menu */
 const menuIcon = document.getElementsByClassName('menu__icon')[0]
 const menuBody = document.getElementsByClassName('menu__navigation')[0]
-// const menuBar = document.getElementsByClassName('icon-menu__bar')
+const ul = document.getElementsByTagName('ul')[0]
+
 
 
 menuIcon.addEventListener('click', () => {
   menuBody.classList.toggle('active');
 	menuIcon.classList.toggle('active');
+})
+
+ul.addEventListener('click', () => {
+  menuBody.classList.remove('active');
+	menuIcon.classList.remove('active');
 })
